39182f1040ac9f255bb86b232368b9e5e009778d
vladimir.nikolaev <vladimir.nikolaev.666@gmail.com>
2016-07-05 00:24:28 +0300

    create new logic: try using calc as object

--- calculator.js
+++ calculator.js
@@ -1,21 +1,147 @@
-var Calculator = function (opt) {
+var Calculator;
+var tmps = {
+    first: [],
+    second: []
+};
+Calculator = function (opt) {
     var that = this;
+    // console.log(this.getButton());
     this.$el = $(opt.el);
-    this.getButton().map(function(item){
-        var $item = $(item);
-        if(that[$item.data('d')] ) {
-            that.on.call(item, 'click', that[$(item).data('d')])
+    this.getButton().map(function (item) {
+        var $item = $(this);
+        if (that[$item.data('button')]) {
+            that.on.call($item, 'click', function (evt) {
+                // console.log('я сработал');
+                that[$item.data('button')]();
+            });
         }
-    })
+    });
 
 
 };
+
 Calculator.prototype.getButton = function () {
     return this.$el.find('.b-buttons .item');
 };
+
 Calculator.prototype.on = function (event, callback) {
     $(this).on(event, callback);
 };
-Calculator.prototype.pisca = function () {
+Calculator.prototype.showResult = function (dec, isNotNum) {
+    console.log(dec);
+    // this.$el.find('#result-string').val(); // строка вывода результата
+    var haveDot = false;
+    var resultStr = this.$el.find('#result-string').val();
+    if (isNotNum) {
+        for (var i = 0; i < resultStr.length; i++) {
+            if (resultStr[i] === dec) {
+                haveDot = true;
+                break;
+            }
+        }
+        if (!haveDot) {
+            this.$el.find('#result-string').val(resultStr + dec);
+        }
+    } else {
+        resultStr === '0' ? this.$el.find('#result-string').val(dec) : this.$el.find('#result-string').val(resultStr + dec);
+    }
 
-}
+};
+
+Calculator.prototype.getNumbers = function (dec, isNotNum) {
+    if (!isNotNum) {
+        tmps.first.push(dec);
+    } else if (tmps.second === []) {
+        tmps.second = tmps.first;
+    } else {
+
+    }
+};
+
+Calculator.prototype.nine = function () {
+    this.showResult('9', false);
+};
+
+Calculator.prototype.eight = function () {
+    this.showResult('8', false);
+};
+
+Calculator.prototype.seven = function () {
+    this.showResult('7', false);
+};
+
+Calculator.prototype.six = function () {
+    this.showResult('6', false);
+};
+
+Calculator.prototype.five = function () {
+    this.showResult('5', false);
+};
+
+Calculator.prototype.four = function () {
+    this.showResult('4', false);
+};
+
+Calculator.prototype.three = function () {
+    this.showResult('3', false);
+};
+
+Calculator.prototype.two = function () {
+    this.showResult('2', false);
+};
+
+Calculator.prototype.one = function () {
+    this.showResult('1', false);
+};
+
+Calculator.prototype.zero = function () {
+    this.showResult('0', false);
+};
+
+Calculator.prototype.dote = function () {
+    this.showResult('.', true);
+};
+
+Calculator.prototype.sum = function () {
+    this.showResult('+', true);
+};
+
+Calculator.prototype.subtraction = function () {
+    this.showResult('-', true);
+};
+
+Calculator.prototype.delenie = function () {
+    this.showResult('/', true);
+};
+
+Calculator.prototype.multiplic = function () {
+    this.showResult('*', true);
+};
+
+Calculator.prototype.delete = function () {
+    // this.showResult('', true);
+};
+
+Calculator.prototype.sqrt = function () {
+    // this.showResult('+', true);
+};
+
+Calculator.prototype.percent = function () {
+    // this.showResult('+', true);
+};
+
+Calculator.prototype.decUnderOne = function () {
+    // this.showResult('+', true);
+};
+
+Calculator.prototype.xStepenY = function () {
+    this.showResult('^', true);
+};
+
+Calculator.prototype.change_polar = function () {
+    // this.showResult('+', true);
+};
+
+Calculator.prototype.count_up = function () {
+    // this.showResult('+', true);
+};
---
+++ old_js.js
@@ -0,0 +1,160 @@
+function updateInputValue(curr) {
+    if (!!app.nowOperator) app.$input.val('0');
+    app.nowOperator = '';
+    var haveDot = false;
+    var resultStr = app.$input.val();
+    switch (curr) {
+        case '0':
+            resultStr === '0' ? app.$input.val(curr) : app.$input.val(resultStr + curr);
+            break;
+        case '.':
+            for (var i = 0; i < resultStr.length; i++) {
+                if (resultStr[i] === '.') {
+                    haveDot = true;
+                    break;
+                }
+            }
+            if (!haveDot)  app.$input.val(resultStr + curr);
+            break;
+        default:
+            resultStr === '0' ? app.$input.val(curr) : app.$input.val(resultStr + curr);
+
+    }
+
+}
+
+function twoValues(firstSum, secondSum, operator) {
+    firstSum = Number(firstSum);
+    secondSum = Number(secondSum);
+    if (app.nowOperator == 'percent') secondSum = firstSum / 100 * secondSum;
+    app.$input.val('');
+    var result;
+    switch (operator) {
+        case 'delenie':
+            if (secondSum !== 0) {
+                result = firstSum / secondSum;
+                app.$input.val(result.toString());
+            } else {
+                alert('Делить на ноль нельзя!');
+                setInitState();
+            }
+            break;
+        case 'multiplic':
+            result = firstSum * secondSum;
+            app.$input.val(result.toString());
+            break;
+        case 'subtraction':
+            result = firstSum - secondSum;
+            app.$input.val(result.toString());
+            break;
+        case 'sum':
+            result = firstSum + secondSum;
+            app.$input.val(result.toString());
+            break;
+        case 'x^y':
+            result = Math.pow(firstSum, secondSum)
+            app.$input.val(result.toString());
+            break;
+        case 'count-up':
+            if (!!result) {
+                app.$input.val(result.toString());
+                app.prevValue = app.$input.val()
+            } else {
+                app.$input.val('0');
+            }
+            app.prevOperator = '';
+            break;
+
+    }
+    app.prevValue = '';
+}
+
+function oneValue(firstSum, operator) {
+    firstSum = Number(firstSum);
+    app.$input.val('');
+    var result;
+    switch (operator) {
+        case 'delete':
+            setInitState();
+            break;
+        case 'sqrt':
+            if (firstSum >= 0) {
+                result = Math.sqrt(firstSum);
+                app.$input.val(result.toString());
+            } else {
+                alert('Нельзя получить корень иррационального числа!');
+                setInitState();
+            }
+            break;
+        case 'change-polar':
+            result = firstSum * -1;
+            app.$input.val(result.toString());
+            break;
+        case 'decUnderOne':
+            result = 1 / firstSum;
+            app.$input.val(result.toString());
+            break;
+    }
+}
+
+function getWay(src) {
+    switch (src) {
+        case 'delete':
+        case 'sqrt':
+        case 'change-polar':
+        case 'decUnderOne':
+            return 'oneWay';
+            break;
+        case 'subtraction':
+        case 'delenie':
+        case 'multiplic':
+        case 'sum':
+        case 'percent':
+        case 'x^y':
+        case 'count-up':
+            return 'twoWays';
+            break;
+    }
+}
+
+
+function setInitState() {
+    app.prevValue = '';
+    app.nowValue = '';
+    app.$input.val('0');
+}
+
+var app = {
+    $container: $('.container'),
+    $input: $('#result-string'),
+    $decimals: $('.item.dec'),
+    $operators: $('.item.operator'),
+    prevValue: '',
+    nowValue: '',
+    prevOperator: '',
+    nowOperator: ''
+};
+
+app.$decimals.on('click', function (evt) {
+    // this.data('d')
+    var number = evt.target.id;
+    updateInputValue(number);
+});
+app.$operators.on('click', function (evt) {
+    var operator = evt.target.id;
+    var checkWays = getWay(operator);
+    if (checkWays == 'twoWays') {
+        if (!!app.prevValue) {
+            app.nowOperator = operator;
+            app.nowValue = app.$input.val();
+            app.$input.val('0');
+            twoValues(app.prevValue, app.nowValue, app.prevOperator);
+        } else {
+            app.prevOperator = operator;
+            app.prevValue = app.$input.val();
+            app.nowOperator = app.prevOperator;
+        }
+    } else {
+        oneValue(app.$input.val(), operator);
+    }
+});
