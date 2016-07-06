e7d071819ae954811afd583c8be144bdebb876c3
vladimir.nikolaev <vladimir.nikolaev@rtlabs.ru>
2016-07-06 18:35:17 +0300

    rework js logic from switch/case to functions

--- calculator.js
+++ calculator.js
@@ -5,7 +5,21 @@ Calculator = function (opt) {
     this.nowOperator = '';
     this.elems = [];
     this.$el = $(opt.el);
-    this.rs = this.$el.find('#result-string').val;
+    this.rs = this.$el.find('#result-string');
+    this.operations = {
+        'delete': this.delete,
+        'sqrt': this.sqrt,
+        'change_polar': this.change_polar,
+        'decUnderOne': this.decUnderOne,
+        '+': this.sum,
+        '-': this.substraction,
+        '/': this.delenie,
+        '*': this.multiple,
+        '%': this.percent,
+        '^': this.xStepenY,
+        '=': this.count_up,
+        '': this.count_up
+    };
     this.getButton().map(function (item) {
         var $item = $(this);
         if (that[$item.data('button')]) {
@@ -28,10 +42,10 @@ Calculator.prototype.on = function (event, callback) {
 };
 
 Calculator.prototype.showResult = function (dec, isNotNum) {
-    var trying = this.rs().split(this.nowOperator);
+    var trying = this.rs.val().split(this.nowOperator);
     // console.log(!!trying[1]);
     var haveSymb = false;
-    var resultStr = !!trying[1] && dec === '.' ? trying[1] : this.rs();
+    var resultStr = !!trying[1] && dec === '.' ? trying[1] : this.rs.val();
     if (isNotNum) {
         for (var i = 0; i < resultStr.length; i++) {
             if (resultStr[i] === dec) {
@@ -40,10 +54,10 @@ Calculator.prototype.showResult = function (dec, isNotNum) {
             }
         }
         if (!haveSymb) {
-            this.rs(this.rs() + dec);
+            this.rs.val(this.rs.val() + dec);
         }
     } else {
-        resultStr === '0' ? this.rs(dec) : this.rs(resultStr + dec);
+        resultStr === '0' ? this.rs.val(dec) : this.rs.val(resultStr + dec);
     }
 
 };
@@ -74,43 +88,44 @@ Calculator.prototype.getResult = function (way, item) {
     var second;
     var result;
     if (way === 'two') {
-        this.elems = this.rs().split(this.nowOperator);
+        this.elems = this.rs.val().split(this.nowOperator);
         first = Number(this.elems[0]);
         second = Number(this.elems[1]);
-        switch (this.nowOperator) {
-            case '/':
-                if (second !== 0) {
-                    result = first / second;
-                    this.rs(result.toString());
-                } else {
-                    alert('Делить на ноль нельзя!');
-                    this.setInitState();
-                }
-                break;
-            case '*':
-                result = first * second;
-                this.rs(result.toString());
-                break;
-            case '-':
-                result = first - second;
-                this.rs(result.toString());
-                break;
-            case '+':
-                result = first + second;
-                this.rs(result.toString());
-                break;
-            case '^':
-                result = Math.pow(first, second);
-                this.rs(result.toString());
-                break;
-            case '%':
-                result = first / 100 * second;
-                this.rs(result.toString());
-            case '=':
-                break;
-        }
+        this.operations[this.nowOperator].call(this, first, second);
+        // switch (this.nowOperator) {
+        //     case '/':
+        //         if (second !== 0) {
+        //             result = first / second;
+        //             this.rs.val(result.toString());
+        //         } else {
+        //             alert('Делить на ноль нельзя!');
+        //             this.setInitState();
+        //         }
+        //         break;
+        //     case '*':
+        //         result = first * second;
+        //         this.rs.val(result.toString());
+        //         break;
+        //     case '-':
+        //         result = first - second;
+        //         this.rs.val(result.toString());
+        //         break;
+        //     case '+':
+        //         result = first + second;
+        //         this.rs.val(result.toString());
+        //         break;
+        //     case '^':
+        //         result = Math.pow(first, second);
+        //         this.rs.val(result.toString());
+        //         break;
+        //     case '%':
+        //         result = first / 100 * second;
+        //         this.rs.val(result.toString());
+        //     case '=':
+        //         break;
+        // }
     } else {
-        first = Number(this.rs());
+        first = Number(this.rs.val());
         switch (item.data('args')) {
             case 'delete':
                 this.setInitState();
@@ -118,7 +133,7 @@ Calculator.prototype.getResult = function (way, item) {
             case 'sqrt':
                 if (first >= 0) {
                     result = Math.sqrt(first);
-                    this.rs(result.toString());
+                    this.rs.val(result.toString());
                 } else {
                     alert('Нельзя получить корень иррационального числа!');
                     this.setInitState();
@@ -126,11 +141,11 @@ Calculator.prototype.getResult = function (way, item) {
                 break;
             case 'change_polar':
                 result = first * -1;
-                this.rs(result.toString());
+                this.rs.val(result.toString());
                 break;
             case 'decUnderOne':
                 result = 1 / first;
-                this.rs(result.toString());
+                this.rs.val(result.toString());
                 break;
 
         }
@@ -139,7 +154,40 @@ Calculator.prototype.getResult = function (way, item) {
 };
 
 Calculator.prototype.setInitState = function () {
-    this.rs('0');
+    this.rs.val('0');
     this.nowOperator = '';
     this.elems = [];
+};
+
+Calculator.prototype.delenie = function (first, second) {
+    if (second !== 0) {
+        this.rs.val((first / second).toString());
+    } else {
+        alert('Делить на ноль нельзя!');
+        this.setInitState();
+    }
+};
+
+Calculator.prototype.multiple = function (first, second) {
+    this.rs.val((first * second).toString());
+};
+
+Calculator.prototype.substraction = function (first, second) {
+    this.rs.val((first - second).toString());
+};
+
+Calculator.prototype.sum = function (first, second) {
+    this.rs.val((first + second).toString());
+};
+
+Calculator.prototype.xStepenY = function (first, second) {
+    this.rs.val(Math.pow(first, second).toString());
+};
+
+Calculator.prototype.percent = function (first, second) {
+    this.rs.val((first / 100 * second).toString());
+};
+
+Calculator.prototype.count_up = function (first, second) {
+    this.rs.val(this.rs.val());
 };
